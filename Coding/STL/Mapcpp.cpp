9f29a2120001f51b2e0ba19d99a82615bc7ccea9
KedarSadhu <53435331+KedarSadhu@users.noreply.github.com>
2024-12-13 13:32:52 +0530

    modifies

--- Coding/STL/Mapcpp.cpp
+++ Coding/STL/Mapcpp.cpp
@@ -24,15 +24,31 @@ int main()
     mp1.insert(pair<int, string>(10, "Kedar")); //C++98
     mp1.insert({2, "Snehal"});//initializer list introduced in C++11
     mp1[12] = "Sadhu";  // C++11 and later
-
+    /*
+        2       Snehal
+        10      Kedar
+        12      Sadhu
+    */
     display(mp1);
 
+    //Trying to insert same key
     mp1.emplace(2, "MR.");  //ignored -> no effect //C++14 and later
     mp1.insert({2, "MR."}); //ignored -> no effect
+    mp1.insert(pair<int, string>(2, "MR.")); //ignored -> no effect
+    /*
+        2       Snehal
+        10      Kedar
+        12      Sadhu
+    */
     display(mp1);
 
     //C++17 ->
     mp1.insert_or_assign(2, "MR."); //insert if key does not exist, or update value if key exists
+    /*
+        2       MR.
+        10      Kedar
+        12      Sadhu
+    */
     display(mp1);
 
     if(mp1.find(5) != mp1.end())
@@ -44,7 +60,11 @@ int main()
         cout <<"\n\nKey not found";
     }
 
-    mp1.erase(3);
+    mp1.erase(2);
+    /*
+        10      Kedar
+        12      Sadhu
+    */
     display(mp1);
 
     return 0;
