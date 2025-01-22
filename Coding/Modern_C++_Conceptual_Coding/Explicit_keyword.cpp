8e44d0b633934ee0f2078de86b7e3b69e2076fc5
KedarSadhu <kedarsadhu@gmail.com>
2025-01-22 21:19:22 +0530

    explicit keyword

--- Coding/Modern_C++_Conceptual_Coding/Explicit_keyword.cpp
+++ Coding/Modern_C++_Conceptual_Coding/Explicit_keyword.cpp
@@ -1,46 +1,45 @@
 #include<iostream>
- 
+
 using namespace std;
 
-
-//Explicit keyword in C++
- 
-//Implicit conversion
- 
-//class demo
-//{
-//  int iNo;
-//public:
-//  demo(int i)
-//  {
-//    iNo = i;
-//  }
-//
-//  int getNo(demo d)
-//  {
-//    return d.iNo;
-//  }
-//};
- 
 class demo
 {
-  int iNo;
-public:
-  demo(int i)
-  {
-    iNo = i;
-  }
- 
-  int getNo(demo d)
-  {
-    return d.iNo;
-  }
+    int iNo;
+  
+    public:
+    demo()
+    {
+        cout<<"Demo()"<<endl;
+    }
+
+    //demo(int i)
+    explicit demo(int i)
+    {
+        iNo = i;
+        cout<<"Demo(int) :: "<< iNo <<endl;
+    }
+
+    void print()
+    {
+        cout<<"iNo = "<<iNo<<endl;
+    }
 };
- 
+
+void fun(demo d)
+{
+  d.print();
+}
+
 int main()
 {
-  demo d(10);
-  cout << d.getNo(10) << endl;  //compiler performs implicit conversion of demo object to int
- 
-  return 0;
+    demo d1(20);
+    demo d2 = 30;   //allowed if demo(int i) is used --> 30//he allow kasa ky zala ?? implcitly constructor call zalya mule he allow zala
+    //he asa behaviour avoid karnya sathi explicit key word use kartat jyane asla code complie ch hot nahi.
+    // Error if explicit demo(int i) is used --> avoid implicit casting
+
+    fun(d1);        //20
+    fun(40);       // Error if explicit demo(int i) is used --> avoid implicit casting
+    //allowed if demo(int i) is used --> implicit call jato parameterized constructor la        40
+
+    return 0;
 }
